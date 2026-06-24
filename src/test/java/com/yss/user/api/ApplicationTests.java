e8bcd911f5e0eef6fe72129db785cc2f4794d13a
YMN <yalchanshop@gmail.com>
2026-06-24 15:25:18 +0300

    Initial project

---
+++ src/test/java/com/yss/user/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yss.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
